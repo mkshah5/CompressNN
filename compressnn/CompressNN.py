b4fac89fbbaa5b07aa677b053af3250e55ceb13d
Milan Shah <mkshah5@ncsu.edu>
2024-11-13 11:54:24 -0500

    Added input shape parameter for robust tracing

--- compressnn/CompressNN.py
+++ compressnn/CompressNN.py
@@ -15,18 +15,19 @@ __init__ Arguments:
 - model: torch.nn.Module to integrate compression with (torch.nn.Module)
 - compressor: compressor name (str)
 - batch_size: Batch size for training (int)
+- input_shape: Shape of input (tuple)
 - config_path: Path to a compression JSON configuration file (str)
 - compress_check: function header that returns true when an activation should be compressed (function)
 - free_space: Frees original and compressed data appropriately (bool)
 - get_debug: Print debug information (bool)
 '''
 class CompressNNModel(nn.Module):
-    def __init__(self, model, batch_size, config_path="./config.json",compress_check=contiguous_float32_check, free_space=True, get_debug=False):
+    def __init__(self, model, batch_size, input_shape, config_path="./config.json",compress_check=contiguous_float32_check, free_space=True, get_debug=False):
         super(CompressNNModel, self).__init__()
         self.internal_model = model
         self.batch_size = batch_size
     
-        self.trace = Tracer(self.internal_model).trace().get_tensor_trace()
+        self.trace = Tracer(self.internal_model, batch_size, input_shape).trace().get_tensor_trace()
         self.composer = Composer(config_path, compress_check, self.trace, free_space, get_debug)
     def forward(self, x):
         self.composer.tcount = 0
--- compressnn/Tracer.py
+++ compressnn/Tracer.py
@@ -3,27 +3,48 @@ sys.path.append("../")
 
 import torch
 import torch.nn as nn
-
+import copy
 '''
 Traces the model to retrieve a mapping of operator number to operator name
 
 __init__ Arguments:
 - model: torch.nn.Module to trace (torch.nn.Module)
 '''
+class TracerModel(nn.Module):
+    def __init__(self, model):
+        super(TracerModel, self).__init__()
+        self.internal_model = copy.deepcopy(model)
+        self.map = dict()
+        self.tcount = 0
+        
+    def forward(self, x):
+        self.internal_model
+
+    def destroy(self):
+        del self.internal_model
+        return
 
 class Tracer():
-    def __init__(self, model):
+    def __init__(self, model, batch_size, input_shape):
         super(Tracer, self).__init__()
-        self.internal_model = model
+        self.internal_model = TracerModel(model)
         self.map = dict()
+        self.module_order = []
+        self.batch_size = batch_size
+        self.input_shape = (batch_size, )+input_shape
+        for name, module in self.internal_model.named_modules():
+            module.register_forward_hook(self.hook)
+    def hook(self,module, input, output):
+        if input.shape[0] == self.batch_size:
+            self.module_order.append(module.__class__.__name__)
 
     def trace(self):
-        i = 0
-        for name, layer in self.internal_model.named_modules():
-            if not isinstance(layer, nn.Sequential):
-                if i != 0:
-                    self.map[i-1] = type(layer).__name__
-                i+=1
+        y = self.internal_model(torch.randn(self.input_shape))
+        print(self.module_order)
+        for i in range(len(self.module_order)):
+            self.map[i] = self.module_order[i]
         return self
     def get_tensor_trace(self):
         return self.map
+    def destroy(self):
+        self.internal_model.destroy()
