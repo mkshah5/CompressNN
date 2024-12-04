6f81a586638abc3cdb84241612498c489a4187a7
mkshah5 <89308999+mkshah5@users.noreply.github.com>
2024-12-04 10:10:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='compressnn',
     author = 'Milan Shah',
     author_email='mkshah5@ncsu.edu',
-    version='0.0.1',
+    version='0.0.3',
     packages=find_packages(),
     install_requires=[
         'torch'
